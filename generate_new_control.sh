bf09c46e06091fa8eda911640f91e795b7c80fee
leschNUX <github@leschneronline.de>
2017-11-07 20:13:02 +0100

    inital

---
+++ generate_new_control.sh
@@ -0,0 +1,13 @@
+#!/bin/bash   
+rm controls_loxone2fhem.txt
+find ./FHEM -type f \( ! -iname ".*" \) -print0 | while IFS= read -r -d '' f; 
+  do
+   echo "DEL ${f}" >> controls_loxone2fhem.txt
+   out="UPD "$(stat -c %y  $f | cut -d. -f1 | awk '{printf "%s_%s",$1,$2}')" "$(stat -c %s $f)" ${f}"
+   echo ${out//.\//} >> controls_loxone2fhem.txt
+done
+
+# CHANGED file
+echo "LedController last change:" > CHANGED
+echo $(date +"%Y-%m-%d") >> CHANGED
+echo " - $(git log -1 --pretty=%B)" >> CHANGED
